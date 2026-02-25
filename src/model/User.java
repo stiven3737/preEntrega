6f2ad79730e3af1f15ef3d39bb21836f6d90902a
stiven <stivengonzalez530@gmail.com>
2026-02-24 20:08:27 -0500

    inicializando

---
+++ src/model/User.java
@@ -0,0 +1,42 @@
+package model;
+
+public  class User {
+   private String name;
+   private int idUser;
+   private String phone;
+   private String email;
+
+    public User()  {
+    }
+
+    public User(String name, int idUser, String, String phone,String email) {
+
+         this.name = name;
+         this.idUser = idUser;
+         this.phone = phone;
+         this.email = email;
+         
+         
+
+
+         
+     
+        }
+        
+
+      
+
+      
+        
+
+        
+
+
+     }
+
+
+
+
+
+
+}
